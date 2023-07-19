c6c10adbb7cce705c83a26207d6b2c7b5e313eb2
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-07-19 20:39:32 +0300

    Added a "Side bar" component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
