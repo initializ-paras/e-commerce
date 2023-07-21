e8df56cf3a1e9660ed46366cefd5919d23962502
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-07-21 20:56:35 +0300

    Updated design of side bar elements

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -4,6 +4,4 @@ import { Component } from '@angular/core';
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html'
 })
-export class SideBarComponent {
-  
-}
+export class SideBarComponent {}
