000589ff99b75a4d75d073fe7c546533fcc005d3
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-10-30 20:41:46 +0200

    Added specification accordion component

---
+++ src/app/components/filters/common/components/specifications-accordion/specifications-accordion.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input} from '@angular/core';
+import {accordionAnimation} from "../../animations/accordion.animations";
+import {FiltersService} from "../../../filters.service";
+
+@Component({
+  selector: 'app-specifications-accordion',
+  templateUrl: './specifications-accordion.component.html',
+  animations: [accordionAnimation],
+})
+
+export class SpecificationsAccordionComponent {
+  @Input() filterData: {[p: string]: {[p: string]: number}} = {};
+  @Input() selectedFilters: string[] = [];
+  @Input() category: string = '';
+
+  constructor(public filterService : FiltersService) {
+  }
+
+  protected readonly Object = Object;
+
+  getSuitableHeading(title : string, attribute : string) : string {
+    return this.filterService.titleMapping[(title.toLowerCase() + ' ' + attribute.toLowerCase()).replaceAll(' ', '_')];
+  }
+}
