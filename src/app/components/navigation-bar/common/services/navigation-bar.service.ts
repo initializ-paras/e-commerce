29b126050aac8343bd5263e2332efdab8ea4f885
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-10-30 20:36:51 +0200

    Added additional method in navigation-bar.service.ts

--- src/app/components/navigation-bar/common/services/navigation-bar.service.ts
+++ src/app/components/navigation-bar/common/services/navigation-bar.service.ts
@@ -11,5 +11,16 @@ export class NavigationBarService {
 
   toggleMenu(): void {
     this.isMenuOpenSubject.next(!this.isMenuOpenSubject.value);
+    this.updateBodyOverflowClass();
+  }
+
+  updateBodyOverflowClass(): void {
+    const body = document.body!;
+    if (body.classList.contains('overflow-y-auto')) {
+      body.classList.replace('overflow-y-auto', 'overflow-y-hidden');
+    }
+    else if (body.classList.contains('overflow-y-hidden')) {
+      body.classList.replace('overflow-y-hidden', 'overflow-y-auto');
+    }
   }
 }
