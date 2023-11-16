8554fa65058619b4deffd579c697e99ba9d53e97
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-11-16 20:19:09 +0200

    Animated modal window for filters

--- src/app/components/catalog/common/components/filter-modal/filter-modal.component.ts
+++ src/app/components/catalog/common/components/filter-modal/filter-modal.component.ts
@@ -1,9 +1,16 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FiltersComponent} from "../../../../filters/filters.component";
+import {CatalogComponent} from "../../../catalog.component";
 import {FiltersService} from "../../../../filters/filters.service";
+import {
+  filterModalBackgroundAnimation,
+  filterModalWindowAnimation
+} from "./filter-modal.animations";
 
 @Component({
   selector: 'app-filter-modal',
-  templateUrl: './filter-modal.component.html'
+  templateUrl: './filter-modal.component.html',
+  animations: [filterModalWindowAnimation, filterModalBackgroundAnimation]
 })
 
 export class FilterModalComponent {
@@ -11,10 +18,20 @@ export class FilterModalComponent {
   @Input() category: string = '';
   @Output() closeModal: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor(public filterService : FiltersService) {
+  constructor(public filterService : FiltersService, private catalogComponent : CatalogComponent) {
   }
 
   onCloseModal() {
     this.closeModal.emit();
+  }
+
+  removeAllFilters() {
+    this.filterService.clearSelectedFilters();
+
+    this.catalogComponent.updateCatalog();
+
+    let filters : FiltersComponent = new FiltersComponent(this.filterService);
+
+    filters.updateFilters(this.category);
   }
 }
