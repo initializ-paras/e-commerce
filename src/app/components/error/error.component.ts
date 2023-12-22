52928b8edf9cfa2703c140f8b7fb6772295d46db
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-12-22 16:50:09 +0200

    Updated the template of the error component

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html'
 })
 
-export class ErrorComponent {
-
-}
+export class ErrorComponent { }
