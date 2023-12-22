ca628f55e5d052e39056b80aab3a8393f659d3a7
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-12-22 16:49:39 +0200

    Changed error route in app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import {ErrorComponent} from "./components/error/error.component";
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'catalog', loadChildren: () => import('./components/catalog/catalog.module').then(m => m.CatalogModule) },
-  { path: 'not-found', component: ErrorComponent },
-  { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
+  { path: 'error', component: ErrorComponent },
+  { path: '**', redirectTo: 'error', pathMatch: 'full' }
 ];
 
 @NgModule({
