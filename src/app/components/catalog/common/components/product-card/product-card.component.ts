931e18ccdf11d898315d9f1d6969af9d6c470f68
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-03 17:11:23 +0200

    Added product route generation and navigation links in product cards

--- src/app/components/catalog/common/components/product-card/product-card.component.ts
+++ src/app/components/catalog/common/components/product-card/product-card.component.ts
@@ -1,7 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {GeneralizedProduct} from "../../../../../modules/shared/models/generalized-product";
 
-
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html'
@@ -9,5 +8,26 @@ import {GeneralizedProduct} from "../../../../../modules/shared/models/generaliz
 
 export class ProductCardComponent {
   @Input() product!: GeneralizedProduct;
-  @Input() url!: string;
+
+  getProductUrlCategory() : string {
+    return this.pluralizeWord(
+      this.product.category.toLowerCase().replaceAll(' ', '_'));
+  }
+
+  pluralizeWord(word: string): string {
+    const pluralRules: [RegExp, string][] = [
+      [/s$/, 's'],
+      [/(ch|sh|ss|x)$/, '$1es'],
+      [/(ay|ey|oy|uy)$/, '$1s'],
+      [/(y)$/, 'ies'],
+    ];
+
+    for (const [rule, replacement] of pluralRules) {
+      if (rule.test(word)) {
+        return word.replace(rule, replacement);
+      }
+    }
+
+    return word + 's';
+  }
 }
