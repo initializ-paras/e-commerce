28acc93a605f35eff8217dc0ed9d9322fc76996e
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-03 17:10:14 +0200

    Updated navigation service, removed unused files

--- src/app/components/navigation-bar/search-bar/search.service.ts
+++ src/app/components/navigation-bar/search-bar/search.service.ts
@@ -1,20 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Pagination} from "../../../modules/shared/models/pagination";
-import {Product} from "../../../modules/shared/models/product";
-import {environment} from "../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class SearchService {
-  baseApiUrl : string = environment.apiUrl;
   searchedText : string = '';
-
-  constructor(private http : HttpClient) { }
-
-  getSearchResults(searchedData : string) {
-    return this.http.get<Pagination<Product[]>>(this.baseApiUrl + "productsearch?text=" + searchedData);
-  }
 }
