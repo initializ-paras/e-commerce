c0128757b1d5358085904bad0500b803162cead0
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-26 11:07:00 +0200

    Added additional auth-related methods in account service

--- src/app/modules/account/account.service.ts
+++ src/app/modules/account/account.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../../environments/environment";
-import {BehaviorSubject, map} from "rxjs";
+import {BehaviorSubject, map, Observable} from "rxjs";
 import {User} from "../shared/models/user";
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
+import {AuthStatus} from "../shared/models/auth-status";
 
 @Injectable({
   providedIn: 'root'
@@ -11,23 +12,27 @@ import {Router} from "@angular/router";
 export class AccountService {
   baseUrl = environment.apiUrl;
   private currentUserSource: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
-  currentUser$ = this.currentUserSource.asObservable();
+  currentUserSource$ = this.currentUserSource.asObservable();
+  private authStatusSource: BehaviorSubject<AuthStatus | null> = new BehaviorSubject<AuthStatus | null>(null);
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {
+  }
 
   login(loginData: any) {
-    return this.http.post<User>(this.baseUrl + "user/login", loginData).pipe(
-      map(user => {
-        localStorage.setItem('userAuthToken', user.token);
-        this.currentUserSource.next(user);
-      })
+    return this.http.post<User>(this.baseUrl + "user/login", loginData, { withCredentials: true })
+      .pipe(
+        map(user => {
+          this.currentUserSource.next(user);
+          this.router.navigateByUrl('/');
+          return;
+        })
     );
   }
 
   logout() {
-    localStorage.removeItem('userAuthToken');
     this.currentUserSource.next(null);
     this.router.navigateByUrl('/');
+    return this.removeAuthCredentials().subscribe();
   }
 
   register(registrationData: any) {
@@ -41,5 +46,32 @@ export class AccountService {
 
   isExistingEmail(email: string) {
     return this.http.get<boolean>(this.baseUrl + "user/emailstatus?email=" + email);
+  }
+
+  loadCurrentUser() {
+    return this.http.get<User>(this.baseUrl + "user/currentuser", { withCredentials: true })
+      .pipe(
+        map(user =>
+          this.currentUserSource.next(user)
+        )
+      );
+  }
+
+  getCurrentUserValue() {
+    return this.currentUserSource.value;
+  }
+
+  getAuthStatus() {
+    return this.http.get<AuthStatus>(this.baseUrl + "user/authstatus", { withCredentials: true });
+  }
+
+  refreshAccessToken(): Observable<string> {
+    return this.http.put<string>(this.baseUrl + "user/RefreshAccessToken", null,
+      { responseType: 'json', withCredentials: true });
+  }
+
+  private removeAuthCredentials() {
+    return this.http.post<string>(this.baseUrl + "user/logout", null,
+      { responseType: 'json', withCredentials: true });
   }
 }
