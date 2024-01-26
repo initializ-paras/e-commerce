369d26611f2d7c24739f1c1a513b16b1f7381700
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-26 11:24:57 +0200

    Refactored app component, added tryAuthenticate method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,45 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 import {GeneralizedProduct} from "./modules/shared/models/generalized-product";
 import {BasketService} from "./components/features/basket/basket.service";
+import {AccountService} from "./modules/account/account.service";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'BuyIt.UI';
-  items : GeneralizedProduct[] = [];
+  items: GeneralizedProduct[] = [];
 
-  constructor(private basketService: BasketService) { }
+  constructor(private basketService: BasketService, private accountService: AccountService) {}
 
   ngOnInit() {
+    this.tryGetBasket();
+    this.tryAuthenticate();
+  }
+
+  private tryAuthenticate() {
+    this.accountService.getAuthStatus().subscribe(authStatus => {
+      if (!authStatus?.containsAccessToken && authStatus?.containsRefreshToken) {
+        this.accountService.refreshAccessToken().subscribe(
+          () => {
+            this.accountService.getAuthStatus().subscribe(
+              updatedAuthStatus => {
+                if (updatedAuthStatus.containsAccessToken && updatedAuthStatus.containsRefreshToken) {
+                  this.accountService.loadCurrentUser().subscribe();
+                }
+              });
+          });
+      }
+      else if (authStatus.containsAccessToken && authStatus.containsRefreshToken) {
+        this.accountService.loadCurrentUser().subscribe();
+      }
+    });
+  }
+
+  private tryGetBasket() {
     const basketId = localStorage.getItem('basketId');
 
     if (basketId) {
@@ -22,3 +47,4 @@ export class AppComponent implements OnInit{
     }
   }
 }
+
